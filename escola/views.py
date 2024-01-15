a88a9b722d7a08e28371bb9d65736d6ae76cd42e
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-01-15 15:01:20 -0300

    Video_3.1

--- escola/views.py
+++ escola/views.py
@@ -1,8 +1,13 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializers import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1, 'nome':'Lais'}
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
 
-# Create your views here.
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
